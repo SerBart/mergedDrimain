d9abf95b36b8dfaf33f3ce9fe9e919f09a2402f3
TBartosz Seredyn <seredyn.b@gmail.com>
2026-01-30 14:37:06 +0100

    Poprawione powiadomienia i dodany temat do zgloszen

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "40795ac6481c354a8a898634b935be13
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "df836247618421e3cf8567e481e70d7a",
+"flutter_bootstrap.js": "148871d945ef44bc066f6701ba3134b3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
