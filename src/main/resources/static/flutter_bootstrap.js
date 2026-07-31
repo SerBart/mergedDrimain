bd8f36b045d672b5668bdee705c1e5812bf529d4
TBartosz Seredyn <seredyn.b@gmail.com>
2026-07-31 16:41:47 +0200

    Poprawa działania aplikacji

--- src/main/resources/static/flutter_bootstrap.js
+++ src/main/resources/static/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "255176447" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "249437398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
