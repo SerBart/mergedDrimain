5d2e90328bf2a43b8f36f3d2b3a3260771ed3404
TBartosz Seredyn <seredyn.b@gmail.com>
2026-07-31 10:36:30 +0200

    Dodanie sekcji "sekcje"

--- src/main/java/drimer/drimain/repository/SekcjaRepository.java
+++ src/main/java/drimer/drimain/repository/SekcjaRepository.java
@@ -7,10 +7,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SekcjaRepository extends JpaRepository<Sekcja, Long> {
-    List<Sekcja> findByMaszyna_IdOrderByNazwaAsc(Long maszynaId);
-    List<Sekcja> findByMaszyna_Dzial_IdOrderByNazwaAsc(Long dzialId);
+    List<Sekcja> findByDzial_IdOrderByNazwaAsc(Long dzialId);
     List<Sekcja> findAllByOrderByNazwaAsc();
-    Optional<Sekcja> findByMaszyna_IdAndNazwaIgnoreCase(Long maszynaId, String nazwa);
-    long countByMaszyna_Id(Long maszynaId);
+    Optional<Sekcja> findByDzial_IdAndNazwaIgnoreCase(Long dzialId, String nazwa);
 }
 
