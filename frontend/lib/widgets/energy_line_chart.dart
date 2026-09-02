c18a68fae117967c36579e5fab564056f02dd8de
TBartosz Seredyn <seredyn.b@gmail.com>
2026-09-02 12:17:09 +0200

    Aktualizacja dashboardu, zeby był bardziej czytelny i kafelki na dashboardzie

--- frontend/lib/widgets/energy_line_chart.dart
+++ frontend/lib/widgets/energy_line_chart.dart
@@ -1,4 +1,5 @@
 import 'dart:math' as math;
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -263,7 +264,7 @@ class _EnergyChartPainter extends CustomPainter {
   }) {
     final painter = TextPainter(
       text: TextSpan(text: text, style: style),
-      textDirection: TextDirection.ltr,
+      textDirection: ui.TextDirection.ltr,
       maxLines: 1,
     )..layout();
     painter.paint(canvas, offset);
